c0819eb1486c5873fc5509ec563ec6fb4ae3355c
msa0683 <msa0683@yahoo.com>
2017-08-13 23:22:57 -0700

    index.html page done

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,1 +1,102 @@
-     * **Main** - contains the main-container div that holds the main layout and navigation. This component should also be able to hold sub-components Search and Saved
+     // * **Main** - contains the main-container div that holds the main layout and navigation. T
+     // his component should also be able to hold sub-components Search and Saved
+// Include React
+var React = require("react");
+
+// Here we include all of the sub-components
+var Results = require("./children/Saved");
+var History = require("./children/Search");
+
+// Helper for making AJAX requests to our API
+var helpers = require("./utils/helpers");
+
+// Creating the Main component
+var Main = React.createClass({
+
+  // Here we set a generic state associated with the number of clicks
+  // Note how we added in this history state variable
+  getInitialState: function() {
+    return { searchTerm: "", results: "", history: [] };
+  },
+
+  // The moment the page renders get the History
+  componentDidMount: function() {
+    // Get the latest history.
+    helpers.getHistory().then(function(response) {
+      console.log(response);
+      if (response !== this.state.history) {
+        console.log("History", response.data);
+        this.setState({ history: response.data });
+      }
+    }.bind(this));
+  },
+
+  // If the component changes (i.e. if a search is entered)...
+  componentDidUpdate: function() {
+
+    // Run the query for the address
+    helpers.runQuery(this.state.searchTerm).then(function(data) {
+      if (data !== this.state.results) {
+        console.log("Address", data);
+        this.setState({ results: data });
+
+        // After we've received the result... then post the search term to our history.
+        helpers.postHistory(this.state.searchTerm).then(function() {
+          console.log("Updated!");
+
+          // After we've done the post... then get the updated history
+          helpers.getHistory().then(function(response) {
+            console.log("Current History", response.data);
+
+            console.log("History", response.data);
+
+            this.setState({ history: response.data });
+
+          }.bind(this));
+        }.bind(this));
+      }
+    }.bind(this));
+  },
+  // This function allows childrens to update the parent.
+  setTerm: function(term) {
+    this.setState({ searchTerm: term });
+  },
+  // Here we render the function
+  render: function() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="jumbotron">
+            <h2 className="text-center">Address Finder!</h2>
+            <p className="text-center">
+              <em>Enter a landmark to search for its exact address (ex: "Eiffel Tower").</em>
+            </p>
+          </div>
+
+          <div className="col-md-6">
+
+            <Form setTerm={this.setTerm} />
+
+          </div>
+
+          <div className="col-md-6">
+
+            <Results address={this.state.results} />
+
+          </div>
+
+        </div>
+
+        <div className="row">
+
+          <History history={this.state.history} />
+
+        </div>
+
+      </div>
+    );
+  }
+});
+
+// Export the component back for use in other files
+module.exports = Main;
--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.static("public"));
 // -------------------------------------------------
 
 // MongoDB Configuration configuration (Change this URL to your own DB)
-mongoose.connect("mongodb://localhost/addmern");
+mongoose.connect("");
 var db = mongoose.connection;
 
 db.on("error", function(err) {
@@ -37,46 +37,6 @@ db.once("open", function() {
 
 // -------------------------------------------------
 
-// Main "/" Route. This will redirect the user to our rendered React application
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/public/index.html");
-});
-
-// This is the route we will send GET requests to retrieve our most recent search data.
-// We will call this route the moment our page gets rendered
-app.get("/api", function(req, res) {
-
-  // We will find all the records, sort it in descending order, then limit the records to 5
-  History.find({}).sort([
-    ["date", "descending"]
-  ]).limit(5).exec(function(err, doc) {
-    if (err) {
-      console.log(err);
-    }
-    else {
-      res.send(doc);
-    }
-  });
-});
-
-// This is the route we will send POST requests to save each search.
-app.post("/api", function(req, res) {
-  console.log("BODY: " + req.body.location);
-
-  // Here we'll save the location based on the JSON input.
-  // We'll use Date.now() to always get the current date time
-  History.create({
-    location: req.body.location,
-    date: Date.now()
-  }, function(err) {
-    if (err) {
-      console.log(err);
-    }
-    else {
-      res.send("Saved Search");
-    }
-  });
-});
 
 // -------------------------------------------------
 
@@ -84,3 +44,9 @@ app.post("/api", function(req, res) {
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
+
+// * `/api/saved` (get) - your components will use this to query MongoDB for all saved articles
+
+//      * `/api/saved` (post) - your components will use this to save an article to the database
+
+//      * `/api/saved` (delete) - your components will use this to delete a saved article in the database
