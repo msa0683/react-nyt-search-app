48e595e5039c42905ff9e44daf850824e82c0278
msa0683 <msa0683@yahoo.com>
2017-08-13 22:32:56 -0700

    Article Schema created

---
+++ models/Article.js
@@ -0,0 +1,19 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var ArticleSchema = new Schema ({
+	title: {
+		type: String
+	},
+	date: {
+		type: Date
+	},
+	url: {
+		type: String
+
+	}
+});
+
+   
+var Article = mongoose.model("Article", ArticleSchema);
+module.exports = Article;
---
+++ public/bundle.js
@@ -0,0 +1,73 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+eval("throw new Error(\"Module build failed: Error: The node API for `babel` has been moved to `babel-core`.\\n    at Object.<anonymous> (/Users/Mariam/Documents/codingbootcamp/homework/react-nyt-search-app/node_modules/babel/index.js:1:69)\\n    at Module._compile (module.js:571:32)\\n    at Object.Module._extensions..js (module.js:580:10)\\n    at Module.load (module.js:488:32)\\n    at tryModuleLoad (module.js:447:12)\\n    at Function.Module._load (module.js:439:3)\\n    at Module.require (module.js:498:17)\\n    at require (internal/module.js:20:19)\\n    at loadLoader (/Users/Mariam/Documents/codingbootcamp/homework/react-nyt-search-app/node_modules/loader-runner/lib/loadLoader.js:13:17)\\n    at iteratePitchingLoaders (/Users/Mariam/Documents/codingbootcamp/homework/react-nyt-search-app/node_modules/loader-runner/lib/LoaderRunner.js:169:2)\\n    at runLoaders (/Users/Mariam/Documents/codingbootcamp/homework/react-nyt-search-app/node_modules/loader-runner/lib/LoaderRunner.js:362:2)\\n    at NormalModule.doBuild (/Users/Mariam/Documents/codingbootcamp/homework/react-nyt-search-app/node_modules/webpack/lib/NormalModule.js:181:3)\\n    at NormalModule.build (/Users/Mariam/Documents/codingbootcamp/homework/react-nyt-search-app/node_modules/webpack/lib/NormalModule.js:274:15)\\n    at Compilation.buildModule (/Users/Mariam/Documents/codingbootcamp/homework/react-nyt-search-app/node_modules/webpack/lib/Compilation.js:149:10)\\n    at moduleFactory.create (/Users/Mariam/Documents/codingbootcamp/homework/react-nyt-search-app/node_modules/webpack/lib/Compilation.js:447:10)\\n    at factory (/Users/Mariam/Documents/codingbootcamp/homework/react-nyt-search-app/node_modules/webpack/lib/NormalModuleFactory.js:241:5)\\n    at applyPluginsAsyncWaterfall (/Users/Mariam/Documents/codingbootcamp/homework/react-nyt-search-app/node_modules/webpack/lib/NormalModuleFactory.js:94:13)\\n    at /Users/Mariam/Documents/codingbootcamp/homework/react-nyt-search-app/node_modules/tapable/lib/Tapable.js:268:11\\n    at NormalModuleFactory.params.normalModuleFactory.plugin (/Users/Mariam/Documents/codingbootcamp/homework/react-nyt-search-app/node_modules/webpack/lib/CompatibilityPlugin.js:52:5)\\n    at NormalModuleFactory.applyPluginsAsyncWaterfall (/Users/Mariam/Documents/codingbootcamp/homework/react-nyt-search-app/node_modules/tapable/lib/Tapable.js:272:13)\\n    at resolver (/Users/Mariam/Documents/codingbootcamp/homework/react-nyt-search-app/node_modules/webpack/lib/NormalModuleFactory.js:69:10)\\n    at process.nextTick (/Users/Mariam/Documents/codingbootcamp/homework/react-nyt-search-app/node_modules/webpack/lib/NormalModuleFactory.js:194:7)\\n    at _combinedTickCallback (internal/process/next_tick.js:73:7)\\n    at process._tickCallback (internal/process/next_tick.js:104:9)\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiIwLmpzIiwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///0\n");
+
+/***/ })
+/******/ ]);
